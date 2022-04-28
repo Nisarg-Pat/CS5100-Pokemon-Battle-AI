cf611e1c9b4272bf0378b077329b5ad584131f30
Nisarg Patel <patel321nisarg@gmail.com>
2022-04-28 01:53:23 -0400

    Minimax and Q-learning implemented

--- gamePlots.py
+++ gamePlots.py
@@ -2,6 +2,7 @@ import random
 
 import Data
 import util
+from AI import Agents, QLearning
 from Controller.BattleController import BattleController
 from Model.AgentEnum import AgentEnum
 from Model.BattleModel import Model
@@ -9,24 +10,34 @@ from Model.Player import Player
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    numGames = 5
+    numGames = 10
     x = [i + 1 for i in range(numGames)]
-    y = {"random": [0 for i in range(numGames)], "minimax": [0 for i in range(numGames)],
+    y = {"random": [0 for i in range(numGames)], "randomSuper": [0 for i in range(numGames)],
+         "minimax": [0 for i in range(numGames)],
          "expectimax": [0 for i in range(numGames)], "qlearning": [0 for i in range(numGames)]}
-    agents = {"random": AgentEnum.RandomAgent, "minimax": AgentEnum.MinimaxAgent,
-         "expectimax": AgentEnum.Expectimax, "qlearning": AgentEnum.QLearningAgent}
-
+    agents = {"random": Agents.RandomAgent(0), "minimax": Agents.MinimaxAgent(0),
+              "randomSuper": Agents.RandomAgentAttackOnlySuperEffective(0),
+         "expectimax": Agents.Expectimax(0), "qlearning": QLearning.ApproximateQAgent(0)}
+    finalProb = {}
+    legend = []
     for q in y:
         count = 0
         for i in range(numGames):
             print(i)
             numPokemon = 3
-            agentTypes = [AgentEnum.Expectimax(0), AgentEnum.RandomAgent(1)]
+            agentTypes = [agents[q], Agents.RandomAgentAttackOnly(1)]
             model = util.generateModel(numPokemon, agentTypes, False)
             BattleController(model).start()
             if model.winner == model.playerList[0]:
                 count += 1
             y[q][i] = float(count) / float(x[i])
         # print(str(x[i])+" "+str(count)+" "+str(y[i]))
+        finalProb[q] = y[q][numGames-1]
         plt.plot(x, y[q])
+        legend.append(q)
+    plt.legend(legend)
+    plt.xlabel("Number of games")
+    plt.ylabel("Winning %")
+    plt.title("Vs Random Agent")
     plt.show()
+    print(finalProb)
