c7a52f45726bc6fff8a76da1b15af968d990ba6e
Nisarg Patel <patel321nisarg@gmail.com>
2022-11-09 14:16:52 -0500

    Refactoring

--- Model/Action.py
+++
@@ -1,5 +0,0 @@
-class Action:
-    QUIT = "Quit"
-    ATTACK = "Attack"
-    SWITCH = "Switch"
-    NONE = "None"
--- temp.py
+++
@@ -1,6 +0,0 @@
-import pandas
-
-import Data
-import util
-
-print(util.getMultiplier("Electric", ["Water", "Flying"]))
